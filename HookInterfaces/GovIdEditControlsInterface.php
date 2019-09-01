b3bc36d62a36f78796f467142b090405fa074b0f
Richard Cissee <ric@richard-cissee.de>
2019-09-01 15:19:24 +0200

    Implement https://github.com/vesta-webtrees-2-custom-modules/vesta_gov4webtrees/issues/1

--- HookInterfaces/GovIdEditControlsInterface.php
+++ HookInterfaces/GovIdEditControlsInterface.php
@@ -6,6 +6,6 @@ use Vesta\Model\GenericViewElement;
 
 interface GovIdEditControlsInterface {
 
-  public function govIdEditControl(?string $govId, string $label, bool $onCreate): GenericViewElement;
+  public function govIdEditControl(?string $govId, string $label, string $placeName, bool $onCreate): GenericViewElement;
 
 }
