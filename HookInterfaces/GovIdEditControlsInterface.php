00c419cdf921db1bb02e415d3090199cd678ad6a
Richard Cissee <ric@richard-cissee.de>
2020-04-20 21:51:40 +0200

    Implement placeholder for modals with custom select2 behavior

--- HookInterfaces/GovIdEditControlsInterface.php
+++ HookInterfaces/GovIdEditControlsInterface.php
@@ -6,6 +6,18 @@ use Vesta\Model\GenericViewElement;
 
 interface GovIdEditControlsInterface {
 
+  public function govIdEditControlSelect2ScriptSnippet(): string;
+          
+  /**
+   * 
+   * @param string|null $govId
+   * @param string $id
+   * @param string $name
+   * @param string $placeName
+   * @param bool $forModal if true, this has to be prepared via ajax-modal-vesta.phtml plus govIdEditControlSelect2ScriptSnippet()!
+   * @param bool $withLabel
+   * @return GenericViewElement
+   */
   public function govIdEditControl(
           ?string $govId, 
           string $id, 
