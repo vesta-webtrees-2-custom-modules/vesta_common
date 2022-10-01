99fe295fad8253837f9dec4770a469f5267d7c95
Richard Cissée <ric@richard-cissee.de>
2022-10-01 20:48:42 +0200

    Release 2.1.7.1.0

--- VestaCommonLibModule.php
+++ VestaCommonLibModule.php
@@ -79,6 +79,8 @@ ModuleCustomInterface, ModuleMetaInterface {
 
         //allow custom modules to add head/body content to admin pages as well
         View::registerCustomView('::layouts/administration', $this->name() . '::layouts/administration');
+        
+        View::registerCustomView('::components/select-with-optgroup', $this->name() . '::components/select-with-optgroup');
     }
 
     //Issue #62
