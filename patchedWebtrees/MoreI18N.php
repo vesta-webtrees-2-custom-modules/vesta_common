0796a8b8eafb17ff339f31d8dd22e14b5e9c4203
Richard Cissée <ric@richard-cissee.de>
2023-05-27 20:31:48 +0200

    I18N utils

--- patchedWebtrees/MoreI18N.php
+++ patchedWebtrees/MoreI18N.php
@@ -13,11 +13,17 @@ class MoreI18N {
         return I18N::translate($message, ...$args);
     }
 
-    //functionally same as I18N::translate,
+    //functionally same as I18N::translateContext,
     //different name prevents gettext from picking this up
     //(intention: use where already expected to be translated via main webtrees)
     public static function xlateContext(string $context, string $message, ...$args): string {
         return I18N::translateContext($context, $message, ...$args);
     }
-
+    
+    //functionally same as I18N::plural,
+    //different name prevents gettext from picking this up
+    //(intention: use where already expected to be translated via main webtrees)
+    public static function plural(string $singular, string $plural, int $count, ...$args): string {
+        return I18N::plural($singular, $plural, $count, $args);
+    }
 }
